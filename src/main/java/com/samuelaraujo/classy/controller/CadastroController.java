f341332da9fdb6dcf2a6c6c00c1b83018f426d03
Samuel Araujo <redwoodsamuel@gmail.com>
2022-10-12 20:36:18 -0400

    Continuada a implementação do upload de fotos

--- src/main/java/com/samuelaraujo/classy/controller/CadastroController.java
+++ src/main/java/com/samuelaraujo/classy/controller/CadastroController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/resources/static/js/editar-anuncio.js
+++ src/main/resources/static/js/editar-anuncio.js
@@ -1,8 +1,11 @@
 const botoesApagar = document.getElementsByClassName("editar-anuncio-imagem-apagar-btn");
 const anuncioThumbnails = Array.from(document.getElementsByClassName("anuncio-imagem-thumbnail"));
-const uploadInput = document.getElementById("upload-input");
+const galeriaContainer = document.getElementById("galeria-container");
+const uploadInputWrapper = document.getElementById("upload-wrapper");
+const uploadInput = document.getElementById("editar-anuncio-upload-input");
 const visualizacaoImagem = document.getElementById("img-principal");
 
+// Configura a seleção da imagem principal.
 anuncioThumbnails.map(thumb => {
     thumb.addEventListener("click", (e) => {
         const imagemClicada = anuncioThumbnails.filter(thumb => e.target.id === thumb.id)[0];
@@ -17,4 +20,32 @@ anuncioThumbnails.map(thumb => {
         }
         
     });
-})
+});
+
+uploadInput.addEventListener("change", (e) => {
+
+    let novaImagemWrapper = document.createElement("div");
+    novaImagemWrapper.className = "d-flex p-3 px-5 justify-content-center border align-items-center ms-2";
+
+    let spinner = document.createElement("span");
+    spinner.className = "spinner-border";
+    spinner.setAttribute("role", "status");
+
+    novaImagemWrapper.appendChild(spinner);
+    
+
+
+    galeriaContainer.insertBefore(novaImagemWrapper, uploadInputWrapper);
+
+    setTimeout(() => {
+        novaImagemWrapper.removeChild(spinner);
+        let novaImagem = document.createElement("img");
+        novaImagem.src = e.target;
+        novaImagem.className = "img-thumbnail";
+        
+        novaImagemWrapper.appendChild(novaImagem);
+
+    }, 2000)
+
+
+});
